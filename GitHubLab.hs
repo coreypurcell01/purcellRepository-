76561ac1c6ec011c7af3cc5640bf90b6c769c764
coreypurcell01 <purcellc693@strose.edu>
2016-09-26 13:58:59 -0400

    Add files via upload

---
+++ GitHubLab.hs
@@ -0,0 +1,19 @@
+Corey Purcell
+GitHub Lab
+9/26/16
+
+1. Using a source code control system is helpful to manage development because it allows multiple people to work on the same thing and also allows a team or group of people to distribute a project and assign different parts to a specific individual. Also, having a source code control system gives the user a place to safely store their work.  
+
+2. The "Watch" button allows you to follow a conversation and be notified when there are new messages. It also allows you to ignore certain conversations as well as give you the option to not be watching any conversations. 
+
+   The star button allows you to favorite a reposirtory or a specific project.
+
+   The fork button allows you to copy as repository and make test it as you please withiout modifying the original work. 
+
+3. The README.md file contained my last name "Purcell" within it so if I had to guess what its meaning is I would say it would be to identify that the work submitted within that repository is mine. 
+
+Creating Repository
+
+1. 
+
+  
